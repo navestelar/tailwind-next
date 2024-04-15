b3dd5ff6cc1f53c8de268ca161077940a9feba77
navestelar <acch.escola@gmail.com>
2024-04-15 16:04:02 -0300

    dark theme

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,23 +1,24 @@
+import { Button } from '@/components/Button'
 import { LogOut } from 'lucide-react'
-import { Button } from '../Button'
 
 export function Profile() {
   return (
-    <div className="flex items-center gap-3">
+    <div className="grid grid-cols-profile items-center gap-3">
       <img
-        src="https://github.com/navestelar.png"
-        className="size-10 rounded-full"
+        src="https://github.com/diego3g.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
       />
-      <div className="flex flex-1 flex-col truncate">
-        <span className="truncate text-sm font-semibold text-zinc-700">
-          Navestelar
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Diego Fernandes
         </span>
-        <span className="truncate text-sm text-zinc-500">
-          navestelar@ntl.com.br
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
+          diego@rocketseat.com.br
         </span>
       </div>
       <Button type="button" variant="ghost">
-        <LogOut className="size-5 text-zinc-500" />
+        <LogOut className="h-5 w-5 text-zinc-500" />
       </Button>
     </div>
   )
