e005717c91932a375459edd6757e05c85155b014
navestelar <acch.escola@gmail.com>
2024-04-15 16:51:06 -0300

    update default informations

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -12,10 +12,10 @@ export function Profile() {
       />
       <div className="flex flex-col truncate">
         <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
-          Diego Fernandes
+          Navestelar
         </span>
         <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
-          diego@rocketseat.com.br
+          navestelar.ntl@gmail.com
         </span>
       </div>
       <Button type="button" variant="ghost">
