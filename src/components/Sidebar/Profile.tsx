b0991ae0d48d059135cec62535b19b777700b6db
navestelar <acch.escola@gmail.com>
2024-04-15 12:02:39 -0300

    tailwind-variants slots

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,4 +1,5 @@
 import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
 
 export function Profile() {
   return (
@@ -15,9 +16,9 @@ export function Profile() {
           navestelar@ntl.com.br
         </span>
       </div>
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+      <Button type="button" variant="ghost">
         <LogOut className="size-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   )
 }
