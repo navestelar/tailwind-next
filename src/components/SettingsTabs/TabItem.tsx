2680a3f85ab60809d802726fe782a2cd5142e8eb
navestelar <acch.escola@gmail.com>
2024-04-15 15:14:42 -0300

    responsividade navigation e formulário

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import * as Tabs from '@radix-ui/react-tabs'
 import { motion } from 'framer-motion'
 
@@ -11,16 +13,16 @@ export function TabItem({ value, title, isSelected = false }: TabItemProps) {
   return (
     <Tabs.Trigger
       value={value}
-      className="group relative px-1 pb-4 text-sm font-medium text-zinc-500 outline-none hover:text-violet-700 data-[state=active]:text-violet-700"
+      className=":px-1 group relative pb-4 text-sm font-medium text-zinc-500 outline-none hover:text-violet-700 data-[state=active]:text-violet-700"
     >
-      <span className="group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+      <span className="whitespace-nowrap rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
         {title}
       </span>
 
       {isSelected && (
         <motion.div
           layoutId="activeTab"
-          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-300"
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700"
         />
       )}
     </Tabs.Trigger>
