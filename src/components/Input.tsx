87bb01d4a76ead46f6df58b9eb0be54d2cbd66e9
navestelar <acch.escola@gmail.com>
2024-04-15 11:43:12 -0300

    button variants

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,7 +11,7 @@ type InputControlProps = ComponentProps<'input'>
 export function Control(props: InputControlProps) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder:text-zinc-600"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 outline-none placeholder:text-zinc-600"
       placeholder="Search"
       {...props}
     />
@@ -23,7 +23,7 @@ type InputRootProps = ComponentProps<'div'>
 export function Root(props: InputRootProps) {
   return (
     <div
-      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
       {...props}
     ></div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,45 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/**/*.tsx",
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr max-content',
-        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)'
-      },
-
-      colors:{
-        violet: {
-          25: '#fcfaff'
-        }
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
 
       borderWidth: {
-        6: '6px'
+        6: '6px',
       },
 
-      maxWidth:{
-        app: '700px'
-      }
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
+      },
+
+      maxWidth: {
+        app: '700px',
+      },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: 'translateY(-2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        // slideUpAndFade: {
+        //   from: { opacity: 1 },
+        //   to: { opacity: 0 },
+        // },
+      },
+
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        // slideUpAndFade: 'slideUpAndFade 1s linear',
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
